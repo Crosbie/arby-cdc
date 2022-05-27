162bc3c41c6c5807314e91bac6e91c69ceeec6ad
Eoin Crosbie <ecrosbie@redhat.com>
2022-05-27 13:55:56 +0100

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+var async = require('async');
+
+var APP_URL = "https://price-api.crypto.com/price/v1/tokens?page=1&limit=20";
+var EXCHANGE_URL = "https://price-api.crypto.com/market/v1/token/SLUG/cdc-pair";
+var appPrices = [];
+var exchangePrices = [];
+var newList = [];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  newList = [];
+  getAppPrices(function(err,data){
+
+    async.each(appPrices,function(coin,cb){
+      getExchangePrice(coin,cb)
+      },
+      function(err){
+        console.log('DONE');
+        newList.sort(function(a, b){
+          return b.priceDiff - a.priceDiff;
+        })
+        res.json(newList);
+      })
+  })    
+    // res.render('index', { title: 'Express' });
+  
+  
+});
+
+
+function getAppPrices(cb){
+  var options
+
+
+  axios
+  .get(APP_URL)
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log('Got '+ res.data.data.length + ' tickers');
+    appPrices = res.data.data;
+
+    // remap Data
+    appPrices = appPrices
+    
+    .filter(item => 
+      item.exchange_tradable && item.app_tradable
+    )
+    .map(function(item, index){
+      var obj = {};
+      // console.log(item);
+      // if(!item.exchange_tradable || !item.app_tradable){
+      //   return;
+      // }
+      obj.symbol = item.symbol;
+      obj.slug = item.slug;
+      obj.appPrice = item.usd_price;
+      return obj;
+    })
+
+    return cb(null, appPrices);
+    
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+function getExchangePrice(coin,cb){
+  var url = EXCHANGE_URL;
+  url = url.replace('SLUG',coin.slug);
+  axios
+  .get(url)
+  .then(res => {
+    // console.log(`statusCode: ${res.status}`);
+    coin.exPrice = res.data.quote_usd_price;
+    coin.priceDiff = percentDiff(coin.appPrice, coin.exPrice);
+    newList.push(coin);
+
+    if(coin.exPrice > coin.appPrice){
+      console.log('APP CHEAPER',coin.symbol);
+    }
+    return cb(null, coin);
+    
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+function percentDiff(a, b) {
+  return  100 * Math.abs( ( a - b ) / ( b ) );
+ }
+
+module.exports = router;
